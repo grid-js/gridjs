947ee2e0a2d055dc2753bdbaaffc718365009275
Afshin Mehrabani <afshin.meh@gmail.com>
2020-04-12 15:40:08 +0100

    chore(tests): table with heaer

--- tests/view/table.test.tsx
+++ tests/view/table.test.tsx
@@ -2,12 +2,24 @@ import { mount } from 'enzyme';
 import { h } from "preact";
 import {Table} from "../../src/view/table";
 import Tabular from "../../src/tabular";
+import Header from "../../src/header";
 
 
 describe('Table component', () => {
   it('should render a table', () => {
     const table = mount(
       <Table data={Tabular.fromArray([[1, 2, 3], ['a', 'b', 'c']])} />
+    );
+
+    expect(table.html()).toMatchSnapshot();
+  });
+
+  it('should render a table with header', () => {
+    const table = mount(
+      <Table
+        data={Tabular.fromArray([[1, 2, 3], ['a', 'b', 'c']])}
+        header={Header.fromArray(['h1', 'h2', 'h3'])}
+      />
     );
 
     expect(table.html()).toMatchSnapshot();
