06d7208e069373333a37bdf3225ac357e136c582
Afshin Mehrabani <afshin.meh@gmail.com>
2020-04-12 18:38:52 +0100

    feat: adding rollup

---
+++ rollup.config.js
@@ -0,0 +1,47 @@
+import typescript from 'rollup-plugin-typescript2';
+import { terser } from 'rollup-plugin-terser'
+import size from 'rollup-plugin-sizes'
+import resolve from '@rollup/plugin-node-resolve';
+
+const globals = {}
+
+export default [
+  {
+    input: 'index.ts',
+    output: {
+      name: 'GridJs',
+      file: 'dist/gridjs.development.js',
+      format: 'umd',
+      sourcemap: true,
+      globals,
+    },
+    plugins: [
+      resolve(),
+      typescript({
+        tsconfig: "tsconfig.json",
+        tsconfigOverride: { compilerOptions : { module: "es2015" } }
+      })
+    ],
+  },
+  {
+    input: 'index.ts',
+    output: {
+      name: 'GridJs',
+      file: 'dist/gridjs.production.min.js',
+      format: 'umd',
+      sourcemap: true,
+      globals,
+    },
+    plugins: [
+      resolve(),
+      typescript({
+        tsconfig: "tsconfig.release.json",
+        tsconfigOverride: { compilerOptions : { module: "es2015" } }
+      }),
+      terser(),
+      size({
+        writeFile: false,
+      }),
+    ],
+  },
+]
