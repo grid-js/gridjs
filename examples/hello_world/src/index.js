f6d9ea105c79032babfb0d4c77fc7658f4450e45
Afshin Mehrabani <afshin.meh@gmail.com>
2020-04-15 18:59:55 +0100

    fix: updating examples

--- examples/hello_world/src/index.js
+++ examples/hello_world/src/index.js
@@ -3,22 +3,22 @@ import { Component } from 'preact';
 import Grid from '@usablica/gridjs';
 
 export default class App extends Component {
-  componentDidMount() {
-    new Grid({
+  render() {
+    const grid = new Grid({
       data: [
         [1, 2, 3],
         ['a', 'b', 'b'],
         ["hello", "world", "!"]
       ],
       header: ['h1', 'h2', 'h3']
-    }).render(document.getElementById('container'));
-  }
+    }).createElement();
 
-  render() {
     return (
 			<div>
         <h1>Hello, World!</h1>
-        <div id="container"/>
+        <div id="container">
+          { grid }
+        </div>
       </div>
     );
   }
