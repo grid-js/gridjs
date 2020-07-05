adb04d8389481905a8a8c78cecef047396b88a55
Afshin Mehrabani <afshin.meh@gmail.com>
2020-07-05 15:00:19 +0100

    fix(search): search should not call store actions when keyword is undefined

--- src/view/plugin/search/search.tsx
+++ src/view/plugin/search/search.tsx
@@ -38,7 +38,8 @@ export class Search extends BaseComponent<SearchConfig & BaseProps, {}> {
 
     if (enabled) {
       // initial search
-      this.actions.search(keyword);
+      if (keyword)
+        this.actions.search(keyword);
 
       this.storeUpdatedFn = this.storeUpdated.bind(this);
       this.store.on('updated', this.storeUpdatedFn);
