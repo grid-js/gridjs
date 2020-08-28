b05eac9cce9fb38293b992181805a3cd4bf07f65
Afshin Mehrabani <afshin.meh@gmail.com>
2020-08-28 11:20:49 +0100

    fix(header): updating the camelCase function

--- src/util/string.ts
+++ src/util/string.ts
@@ -1,6 +1,22 @@
 export function camelCase(str: string): string {
-  return str.replace(/(?:^\w|[A-Z]|\b\w|\s+)/g, function (match, index) {
-    if (+match === 0) return ''; // or if (/\s+/.test(match)) for white spaces
-    return index === 0 ? match.toLowerCase() : match.toUpperCase();
-  });
+  if (!str) return '';
+
+  const words = str.split(' ');
+
+  // do not convert strings that are already in camelCase format
+  if (words.length === 1 && /([a-z][A-Z])+/g.test(str)) {
+    return str;
+  }
+
+  return words
+    .map(function (word, index) {
+      // if it is the first word, lowercase all the chars
+      if (index == 0) {
+        return word.toLowerCase();
+      }
+
+      // if it is not the first word only upper case the first char and lowercase the rest
+      return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+    })
+    .join('');
 }
