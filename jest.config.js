12c6113f797f840a111ec2868aab06101f4e8c2b
Afshin Mehrabani <afshin.meh@gmail.com>
2021-05-26 08:48:31 +0100

    chore: configuring jest to run all tests

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,12 @@
+const exps = require('./package.json').exports;
+const roots = Object.entries(exps)
+  .map((x) => !/legacy|package\.json|^\.\/$/gi.test(x[0]) ? x[0] : null)
+  .filter(x => x);
+
 module.exports = {
   testEnvironment: 'node',
   setupFilesAfterEnv: ['jest-extended'],
+  roots: roots,
   globals: {
     'ts-jest': {
       tsconfig: 'tsconfig.test.json',
