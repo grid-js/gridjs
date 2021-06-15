92f4f99f7d7cf93177f588ba2904ac11a9e507b6
Ted, KIM <ds5apn@gmail.com>
2021-06-16 08:46:53 +0900

    Add new locale (ko_KR)

--- l10n/index.ts
+++ l10n/index.ts
@@ -2,5 +2,6 @@ import esES from './es_ES';
 import frFR from './fr_FR';
 import itIT from './it_IT';
 import trTR from './tr_TR';
+import koKR from './ko_KR';
 
-export { esES, frFR, itIT, trTR };
+export { esES, frFR, itIT, trTR, koKR };
