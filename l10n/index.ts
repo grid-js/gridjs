b11cffb952e5077cc3852f73ad8cd5df410b82f5
Luis Ribeiro <luis.mribeiro@sapo.pt>
2021-06-22 15:20:46 +0100

    Add new locale (pt_PT)

--- l10n/index.ts
+++ l10n/index.ts
@@ -1,7 +1,8 @@
 import esES from './es_ES';
 import frFR from './fr_FR';
 import itIT from './it_IT';
+import ptPT from './pt_PT';
 import trTR from './tr_TR';
 import koKR from './ko_KR';
 
-export { esES, frFR, itIT, trTR, koKR };
+export { esES, frFR, itIT, ptPT, trTR, koKR };
