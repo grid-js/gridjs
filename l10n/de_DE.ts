9254fc1c7b8a4bff51090ef43c764b1e33c6750b
Philipp <philipp.bergamin@gmail.com>
2021-09-16 09:08:08 +0200

    Create de_DE.ts

---
+++ l10n/de_DE.ts
@@ -0,0 +1,22 @@
+export default {
+  search: {
+    placeholder: 'Suche...',
+  },
+  sort: {
+    sortAsc: 'Spalte aufsteigend sortieren',
+    sortDesc: 'Spalte absteigend sortieren',
+  },
+  pagination: {
+    previous: 'Zurück',
+    next: 'Vor',
+    navigate: (page, pages) => `Seite ${page} von ${pages}`,
+    page: (page) => `Seite ${page}`,
+    showing: 'Zeige',
+    of: 'von',
+    to: 'bis',
+    results: 'Resultaten',
+  },
+  loading: 'Lade...',
+  noRecordsFound: 'Keine passenden Eintröge gefunden',
+  error: 'Beim Abrufen der Daten ist ein Fehler aufgetreten',
+};
