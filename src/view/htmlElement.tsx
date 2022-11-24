f00eccb9e21727d2ba36120d76c2f95861ee6fb6
Afshin Mehrabani <afshin.meh@gmail.com>
2022-11-24 14:44:45 +0000

    feat: functional HTMLElement

--- src/view/htmlElement.tsx
+++ src/view/htmlElement.tsx
@@ -1,20 +1,10 @@
 import { h } from 'preact';
 
-import { BaseComponent, BaseProps } from './base';
-
-export interface HTMLContentProps extends BaseProps {
+export function HTMLElement(props: {
   content: string;
   parentElement?: string;
-}
-
-export class HTMLElement extends BaseComponent<HTMLContentProps> {
-  static defaultProps = {
-    parentElement: 'span',
-  };
-
-  render() {
-    return h(this.props.parentElement, {
-      dangerouslySetInnerHTML: { __html: this.props.content },
-    });
-  }
+}) {
+  return h(props.parentElement || 'span', {
+    dangerouslySetInnerHTML: { __html: props.content },
+  });
 }
